eef5c2b2f2dd0f22c68228aa24e0d109192d5cdb
John Barnette <jbarnette@gmail.com>
2010-11-11 12:23:07 -0600

    Fix test.

--- test/test_basis_template.rb
+++ test/test_basis_template.rb
@@ -3,7 +3,7 @@ require "basis/template"
 
 class BasisTemplateTest < MiniTest::Unit::TestCase
   def test_initialize
-    t = Basis::Template.new "foo/bar"
-    assert_equal "#{File.expand_path '.'}/foo/bar", t.srcdir
+    t = Basis::Template.new "test/fixtures/empty"
+    assert_equal "#{File.expand_path '.'}/test/fixtures/empty", t.srcdir
   end
 end
